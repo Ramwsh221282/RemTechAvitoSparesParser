b2ab0e6ac20ddec2ac1456ebbba868d5b9ff0dd1
Ramwsh221282 <jimkrauz@gmail.com>
2025-12-09 16:42:32 +0700

    added data structures

--- AvitoSparesParser/Tests/ParsingTests/SparesParserTests.cs
+++ AvitoSparesParser/Tests/ParsingTests/SparesParserTests.cs
@@ -1,16 +1,33 @@
-﻿namespace Tests.ParsingTests;
+﻿using System.Text.Json;
 
-public sealed class RetryCounter(int initial)
+namespace Tests.ParsingTests;
+
+public sealed class RetryCounter(int counter)
 {
-    public int Counter { get; private set; } = initial;
-    
-    public void Increase() => Counter += 1;
+    public int Value { get; private set; } = counter;
+    public void Increase()
+    {
+        int current = Value;
+        int next = current + 1;
+        Value = next;
+    }
 }
 
-public sealed class ProcessedMarker(bool value)
+public interface IRetriable
 {
-    public bool Processed { get; private set; } = value;
+    RetryCounter Counter { get; }
+    void IncreaseRetry() => Counter.Increase();
+}
 
+public interface IProcessMarkable
+{
+    ProcessedMarker Marker { get; }
+    void MarkProcessed() => Marker.MarkProcessed();
+}
+
+public sealed class ProcessedMarker(bool processed)
+{
+    public bool Processed { get; private set; } = processed;
     public void MarkProcessed()
     {
         if (Processed)
@@ -19,26 +36,53 @@ public sealed class ProcessedMarker(bool value)
     }
 }
 
-public sealed class AvitoCatalogueItemMetadata
+public sealed class AvitoCatalogueItemMetadata(string id, string url, string oem)
 {
-    
+    public string Id { get; } = id;
+    public string Url { get; } = url;
+    public string Oem { get; } = oem;
 }
 
+public sealed class PlainJsonStringArray
+{
+    public Lazy<IReadOnlyList<string>> Array { get; }
+    public Lazy<string> Json { get; }
+
+    public PlainJsonStringArray(JsonDocument document)
+    {
+        Array = new Lazy<IReadOnlyList<string>>(() => StringFromJson(document));
+        Json = new Lazy<string>(() => PlainJsonFromDocument(document));
+    }
+
+    private static string PlainJsonFromDocument(JsonDocument document)
+    {
+        return document.RootElement.GetRawText();
+    }
+    
+    private static IReadOnlyList<string> StringFromJson(JsonDocument document)
+    {
+        int length = document.RootElement.GetArrayLength();
+        List<string> strings = new(length);
+        foreach (JsonElement @string in document.RootElement.EnumerateArray())
+            strings.Add(@string.GetString()!);
+        return strings;
+    }
+}
+
+public sealed record PriceInformation(long Value, bool IsNds);
+
 public sealed record AvitoCatalogueSpare(
-    string Id,
-    string Url,
-    string Oem,
-    bool WasProcessed,
-    int RetryCount
-    );
+    AvitoCatalogueItemMetadata Metadata, 
+    RetryCounter Counter, 
+    ProcessedMarker Marker) : 
+    IRetriable, 
+    IProcessMarkable;
 
 public sealed record AvitoSpare(
-    string Id,
-    string Url,
+    AvitoCatalogueSpare Spare,
+    PriceInformation Price,
+    PlainJsonStringArray Photos,
     string Type,
-    string Oem,
-    long Price,
-    bool IsNds,
     string Address);
 
 
